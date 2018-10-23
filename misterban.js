184c539f6febffaa5046d41423ee1ca3f833d19c
Nekspsy <neks.ps@gmail.com>
2018-10-23 18:05:16 +0300

    upd

--- misterban.js
+++ misterban.js
@@ -5,27 +5,12 @@ const request = require('request');
 const token = '791354621:AAHmiTzF4xP8ZjhnMdoeKrPMhuP2FBuRGGU';
 
 // Create a bot that uses 'polling' to fetch new updates
-// const bot = new TelegramBot(token, { polling: true });
-
-const bot = new TelegramBot(token, {
-  polling: true,
-  request: {
-		agentClass: Agent,
-		agentOptions: {
-			socksHost: "206.189.8.85",
-			socksPort: "1080",
-			// If authorization is needed:
-			// socksUsername: process.env.PROXY_SOCKS5_USERNAME,
-			// socksPassword: process.env.PROXY_SOCKS5_PASSWORD
-		}
-	}
-});
+const bot = new TelegramBot(token, { polling: true });
 
 
 	var timerId = setInterval(function() {
 		const chatId = -1001109327125;
-		// 201167093                     - chat id со мной в личке
-		// -1001109327125 или 540366663  - космодром
+
 		let md = `
 		⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️
 		👋 Привет вновь прибывшим. 
@@ -37,7 +22,7 @@ const bot = new TelegramBot(token, {
 		`;
 	
 		bot.sendMessage(chatId, md);
-		// bot.sendMessage(chatId, md, {parse_mode: 'Markdown'});
+		
 	}, 900000);
 
 
